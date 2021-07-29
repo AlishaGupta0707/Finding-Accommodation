ab0fb7beeeced3ec7a6d02cec6541d3e8589ab15
Alisha Gupta <alishaguptaag0707@gmail.com>
2021-07-29 15:31:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    
+	<title>Finding Accomodation</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<script type="text/javascript" >
+	
+	</script>
+</head>
+<body>
+	<div id="Home-Container">
+		<div class="Main-first">
+			<div id="logo"> FA </div>
+			<a href="./login.html" ><button id="Login-Signup">Login</button></a>
+		
+	</div>
+	<div class="Main-Menu">
+		<a href="#Home-Container">Home</a>
+		<a href="#About">About</a>
+		<a href="#City">City</a>
+		<a href="#Contact">Contact</a>
+	</div>
+	<div class="Searching">
+		<div id="SeacrhBox">
+			<h1 id="PARA1">Get Your Dream House!!</h1>
+			<select class="DropBox" id="Price">
+				<option value="Price">Location.. </option>
+				<option value="Dehradun">Dehradun</option>
+				<option value="Roorkee">Roorkee </option>
+				<option value="Haridwar">Haridwar</option>
+				<option value="Haldwani">Haldwani</option>
+				<option value="Rishikesh">Rishikesh </option>
+			</select>
+			<select class="DropBox" id="Category">
+				<option value="Category" >Category.. </option>
+				<option value="Flat">Flat </option>
+				<option value="PG">PG </option>
+				<option value="Hostel">Hostel </option>
+			</select>
+			<select class="DropBox" id="Type">
+				<option value="Type">Type.. </option>
+				<option value="1BHK">1BHK </option>
+				<option value="2BHK">2BHK </option>
+				<option value="3BHK">3BHK </option>
+				<option value="Single">Single </option>
+				<option value="Shared">Shared </option>
+			</select>
+			
+			<br>
+			<a href="Acco.php">
+			<button type="button" class="search">Search</button></a>
+			
+			<p style="color: white; text-align: center; font-size: x-large;">Get your best deal in Uttarakhand</p>
+		</div>
+	</div>
+	<div class="mid-home">
+		<div class="land">
+			<span class="landlord">Are You Landlord?</span> 
+			<a href="./Sell.php" ><button id="Sell">Sell</button></a>
+		</div>
+	</div>
+	<div class="foot-line">
+		<div id="About">
+			<h2>About Us</h2>
+			<p>" Finding Accomodation helps you to find your perfect place in a much faster and newer manner. We understand that while buying or renting a property there are a lot of factors to be taken care of  like water supply electricity supply, preferred area, budget and other amenities. Our website is the destination where you will end up finding the best suitable property. Whether you are looking for a rented property or planning to build your house you can find anything and everything at our portal. Whether you are looking for pg, flat, villa or hostel we provide you a variety of options in your preferred area to choose from. "</p>
+		
+	</div></div>
+</div>
+</body>
+</html>
